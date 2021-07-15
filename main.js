f27b8bce04a64170fbccf3a0be55e0020d44b70c
Marufjon94f <faith94nazarov@yandex.ru>
2021-07-15 11:45:01 +0500

    add

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("Welcom to tour agency");
+
+var elHeader=document.querySelector(".title");
+var a = prompt ("Insert your budget");
+
+if (a >= 750*9433.34 + 120*10354.03 ){
+    console.log (elHeader.textContent="Have a nice flight :)");
+} else 
+    console.log (elHeader.textContent="please, stay tuned for a bit :(");
